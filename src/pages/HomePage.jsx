3925e0127ab0e8c88b5380de2a0afa3b8d8384ab
vermaji <rajaditya14032002@gmail.com>
2024-04-07 00:11:04 +0530

    fifth section done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import Rect from "../assets/Rectangle 192.png";
 import bike from "../assets/bike.png";
 import frect from "../assets/f_rect.png";
 import srect from "../assets/s_rect.png";
+import line from "../assets/line.png";
 import trect from "../assets/t_rect.png";
 import circle from "../assets/circle.png";
 import blue from "../assets/blueline.png";
@@ -189,20 +190,128 @@ const HomePage = () => {
           </div>
         </div>
         <div className="w-1/2 ml-16 flex items-center justify-between gap-4 flex-wrap">
-          <div className="w-[40%] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
+          <div className="w-[40%] text-[#74B9FF] font-[600] text-[23px] leading-[35px] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
             Free PickUp Drop
           </div>
-          <div className="w-[40%] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
+          <div className="w-[40%] text-[#74B9FF] font-[600] text-[23px] leading-[35px] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
             Genuine Parts
           </div>
-          <div className="w-[40%] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
+          <div className="w-[40%] text-[#74B9FF] font-[600] text-[23px] leading-[35px] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
             30 Days Warranty
           </div>
-          <div className="w-[40%] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
+          <div className="w-[40%] text-[#74B9FF] font-[600] text-[23px] leading-[35px] h-[64px] text-center rounded-[10px] flex items-center justify-center bg-[#F5FAFF] border-[1px] border-[#74B9FF]">
             Affordable Prices
           </div>
         </div>
       </div>
+      {/* ************************ */}
+      <div className="fifth_section pt-16 gap-16 flex flex-col pl-5">
+        <div className="w-[50%] gap-5 flex">
+          <div className="w-[46px]">
+            <img src={Rect} alt="" />
+          </div>
+          <div className="flex gap-3 flex-col">
+            <div className=" w-[914px] h-[50px] font-[600] font-poppins text-[40px] overflow-hidden leading-[50px] ">
+              How GoMechanic Works?
+            </div>
+            <div className="font-poppins font-[400] w-[784px] h-[81px] text-[18px] leading-[27px] text-[#6B6B6B]">
+              Lorem Ipsum s simply dummy text of the printing and typesetting
+              industry.orem Ipsum has been the industry standard dummy Lorem
+              Ipsum isimply dummy.
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 ml-16 flex flex-col items-start justify-between gap-16">
+          <div className="flex items-center gap-3">
+            <div className="w-[117px] h-[117px] flex items-center justify-center border-[3px] border-[#74B9FF]  rounded-full bg-[#EBF5FF]">
+              <p className="font-[600] text-[33px] leading-[50px] ">1</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="font-[600] text-[20px] leading-[30px]">
+                Select The Perfect Car Services
+              </p>
+              <p className="font-[400] text-[16px] leading-[24px] text-[#6B6B6B]">
+                From GoMechanics Brand Portfolio Of Services
+              </p>
+            </div>
+          </div>
+          <div className="border border-green-500 -mt-12 -mb-14 ml-14">
+            <img className="h-[100px] bg-[#74B9FF]" src={line} alt="" />
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="w-[117px] h-[117px] flex items-center justify-center border-[3px] border-[#74B9FF]  rounded-full bg-[#EBF5FF]">
+              <p className="font-[600] text-[33px] leading-[50px] ">2</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="font-[600] text-[20px] leading-[30px]">
+                Schedule Free Doorstep Pick-Up
+              </p>
+              <p className="font-[400] text-[16px] leading-[24px] text-[#6B6B6B]">
+                We Offer Free pick up and drop for all services booked
+              </p>
+            </div>
+          </div>
+          <div className="border border-green-500 -mt-12 -mb-14 ml-14">
+            <img className="h-[100px] bg-[#74B9FF]" src={line} alt="" />
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="w-[117px] h-[117px] flex items-center justify-center border-[3px] border-[#74B9FF]  rounded-full bg-[#EBF5FF]">
+              <p className="font-[600] text-[33px] leading-[50px] ">3</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="font-[600] text-[20px] leading-[30px]">
+                Track Your Car Service Real-Time
+              </p>
+              <p className="font-[400] text-[16px] leading-[24px] text-[#6B6B6B]">
+                We Will take Care of Everything from here
+              </p>
+            </div>
+          </div>
+          <div className="border border-green-500 -mt-12 -mb-14 ml-14">
+            <img className="h-[100px] bg-[#74B9FF]" src={line} alt="" />
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="w-[117px] h-[117px] flex items-center justify-center border-[3px] border-[#74B9FF]  rounded-full bg-[#EBF5FF]">
+              <p className="font-[600] text-[33px] leading-[50px] ">4</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="font-[600] text-[20px] leading-[30px]">
+                Earn While We Service
+              </p>
+              <p className="font-[400] text-[16px] leading-[24px] text-[#6B6B6B]">
+                Spead the word! You get Rs.750 Your Friends get Rs.750!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-[45%] ml-16 flex translate-x-16 gap-16">
+          <div className="w-[167px] justify-center items-center rounded-[10px] flex flex-col h-[64px] bg-[#74B9FF]">
+            <p className="font-[600] text-[20px] leading-[30px] text-white">
+              3 Million +
+            </p>
+            <p className="font-[600] text-[13px] leading-[20px]">Cars served</p>
+          </div>
+          <div className="w-[167px] justify-center items-center rounded-[10px] flex flex-col h-[64px] bg-[#74B9FF]">
+            <p className="font-[600] text-[20px] leading-[30px] text-white">
+              3 Million +
+            </p>
+            <p className="font-[600] text-[13px] leading-[20px]">Cars served</p>
+          </div>
+          <div className="w-[167px] justify-center items-center rounded-[10px] flex flex-col h-[64px] bg-[#74B9FF]">
+            <p className="font-[600] text-[20px] leading-[30px] text-white">
+              3 Million +
+            </p>
+            <p className="font-[600] text-[13px] leading-[20px]">Cars served</p>
+          </div>
+          <div className="w-[167px] justify-center items-center rounded-[10px] flex flex-col h-[64px] bg-[#74B9FF]">
+            <p className="font-[600] text-[20px] leading-[30px] text-white">
+              3 Million +
+            </p>
+            <p className="font-[600] text-[13px] leading-[20px]">Cars served</p>
+          </div>
+        </div>
+      </div>
+      {/* *********************** */}
     </div>
   );
 };
